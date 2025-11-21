e033930ebd90f6200067018c07d6ad52861add65
runoneall <runoobsteve@hotmail.com>
2025-11-21 23:55:23 +0800

    update README.md

--- cli/cmd.go
+++ cli/cmd.go
@@ -4,7 +4,7 @@ import "github.com/spf13/cobra"
 
 var Cmd = &cobra.Command{
 	Use:   "onboot",
-	Short: "onboot 是一个现代化的跨平台服务管理器",
+	Short: "onboot 是一个简约的跨平台服务管理器",
 }
 
 func Init() *cobra.Command {
